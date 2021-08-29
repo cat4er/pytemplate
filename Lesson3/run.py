aca5d6d8a0f5c14d86d0498069fcd886297e0691
cat4er <mooncat4er@gmail.com>
2021-08-29 18:42:49 +0300

    H/W of Lesson3
    - добавил шаблонизатор в templator.py
    - добавил шаблоны в pages

---
+++ Lesson3/run.py
@@ -0,0 +1,10 @@
+from wsgiref.simple_server import make_server
+from urls import fronts, routes
+from my_framework.main import Framework
+
+
+application = Framework(routes, fronts)
+
+with make_server('', 8080, application) as httpd:
+    print("Serving on port 8080...")
+    httpd.serve_forever()
